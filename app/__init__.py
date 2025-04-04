ea6cf27857666e0fc91df27c0e26b7e14972201b
yaokisan <yaoki.kobayashi@empire-art.jp>
2025-04-05 00:29:45 +0900

    Fix: Move Flask app instance to app package for Gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,6 @@
 # app パッケージの初期化ファイル 
+# Flaskアプリケーションをパッケージからインポートできるようにする
+from flask import Flask
+app = Flask(__name__, 
+            template_folder='templates',
+            static_folder='static') 
